32b0dceb47dffc6faeba9b4d65f12e710d1e0db3
Arikalp <sankalpkrsaini@gmail.com>
2026-01-15 12:13:17 +0530

    solved image processing error

--- server/src/main/java/com/SpringWebPro/SpringWebPro/models/ProductModel.java
+++ server/src/main/java/com/SpringWebPro/SpringWebPro/models/ProductModel.java
@@ -34,14 +34,7 @@ public class ProductModel {
 
     private Date releaseDate;
 
-    private String imageName;
-
-    private String imageType;
-
-    private byte[] imageData;
-
-
-    //private String imageUrl;
+    private String imageUrl;
 
 
 }
