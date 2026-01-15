df3b8f86896b3d3e7ca30c13e4e235e8948f70b4
Arikalp <sankalpkrsaini@gmail.com>
2026-01-15 12:58:28 +0530

    Search feature error resolved

--- server/src/main/java/com/SpringWebPro/SpringWebPro/repository/ProductRepo.java
+++ server/src/main/java/com/SpringWebPro/SpringWebPro/repository/ProductRepo.java
@@ -2,11 +2,13 @@ package com.SpringWebPro.SpringWebPro.repository;
 
 import com.SpringWebPro.SpringWebPro.models.ProductModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ProductRepo extends JpaRepository<ProductModel, Integer> {
+    @Query("SELECT p FROM ProductModel p WHERE LOWER(p.ProdName) LIKE LOWER(CONCAT('%', :keyword, '%'))")
     List<ProductModel> findByProdNameContainingIgnoreCase(String keyword);
 }
